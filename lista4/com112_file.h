ff91d977d3e16f7ce77ed7f2b1c79bb12c833f2c
leal <gabriellealttrh@gmail.com>
2020-06-01 08:43:45 -0300

    lista 6

--- lista4/com112_file.h
+++ lista4/com112_file.h
@@ -1,8 +1,8 @@
 #ifndef COM112_FILE
 #define COM112_FILE
 
-void saida(int vetor[], int n, int sort);
-void save(int vetor[], int n);
-void relatorio_file(char *sort, double dados[],int len);
+void output(int vetor[], int n, int sort);
+void input(int vetor[], int n);
+// void reportFile(char *sort, double dados[],int len);
 
 #endif
